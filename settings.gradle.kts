5691f0a7c27152c145981a9ed24d3b05a69b5fa7
Johannes-Cloud-lab <flohsabine0@gmail.com>
2026-08-17 22:20:29 +0000

    Gradle Wrapper generiert

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,22 @@
 pluginManagement {
-        repositories {
-                    google()
-                            mavenCentral()
-                                    gradlePluginPortal()
-        }
-}
-dependencyResolutionManagement {
-        repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
             repositories {
                         google()
                                 mavenCentral()
+                                        gradlePluginPortal()
             }
 }
+dependencyResolutionManagement {
+            repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+                repositories {
+                                google()
+                                        mavenCentral()
+                }
+}
 
-rootProject.name = "MyNativeLauncher"
+rootProject.name = "MeinLauncher"
 include(":app")
 
-            }
+                }
 }
-        }
+            }
 }
